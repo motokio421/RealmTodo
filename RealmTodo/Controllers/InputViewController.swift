92e264839d56ed68c9bc6e5c8573800962f4295b
motoki <higamotoki5731@gmail.com>
2019-07-31 11:52:32 +0800

    一覧機能完成

--- RealmTodo/Controllers/InputViewController.swift
+++ RealmTodo/Controllers/InputViewController.swift
@@ -1,30 +1,83 @@
-//
+
 //  InputViewController.swift
 //  RealmTodo
 //
-//  Created by 比嘉幹樹 on 2019/07/30.
-//  Copyright © 2019 比嘉幹樹. All rights reserved.
+//  Created by yonekan on 2019/07/30.
+//  Copyright © 2019 yonekan. All rights reserved.
 //
 
 import UIKit
+import RealmSwift
 
 class InputViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    //前の画面から渡されてきたTODOを受け取る変数
+    var todo: Todo? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    fileprivate func createNewTodo(_ text: String) {
+        // Realmに接続
+        let realm = try! Realm()
+        
+        // データを登録する
+        let todo = Todo()
+        // 最大のIDを取得
+        let id = getMaxId()
+        
+        todo.id = id
+        todo.title = text
+        todo.date = Date()
+        
+        //        作成したTODOを登録する
+        try! realm.write{
+            realm.add(todo)
+        }
     }
-    */
-
+    
+    @IBAction func didClickButton(_ sender: UIButton) {
+        
+        // nilかチェックをする
+        guard let text = textField.text else {
+            // textField.textがnilの場合
+            // ボタンがクリックされた時の処理を中断
+            return
+        }
+        
+        if text.isEmpty {
+            // textField.textが空文字の場合
+            // ボタンがクリックされた時の処理を中断
+            return
+        }
+        
+//        新規タスクを追加
+        createNewTodo(text)
+        
+//        前の画面に戻る
+        navigationController?.popViewController(animated :true)
+    }
+    
+    
+    // 最大IDを取得するメソッド
+    func getMaxId() -> Int {
+        // Realmに接続
+        let realm = try! Realm()
+        
+        // Todoのシートから最大のIDを取得する
+        let id = realm.objects(Todo.self).max(ofProperty: "id") as Int?
+        
+        if id == nil {
+            // 最大IDがnil(存在しない)場合、1を返す
+            return 1
+        } else {
+            // 最大IDが存在する場合、最大ID + １を返すr
+            return id! + 1
+        }
+    }
+    
+    
 }
--- RealmTodo/Controllers/ViewController.swift
+++ RealmTodo/Controllers/ViewController.swift
@@ -7,15 +7,35 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
     
     
     @IBOutlet weak var tableView: UITableView!
     
+    var todos: [Todo] = []
+    
+    fileprivate func roloadTableView() {
+        //  realmに接続する
+        let realm = try! Realm()
+        
+        //  TODoの一覧を取得
+        todos = realm.objects(Todo.self).reversed()
+        tableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+//    画面が表示されるたびに実行
+    override func viewWillAppear(_ animated: Bool) {
+        roloadTableView()
     }
 
     
@@ -26,3 +46,42 @@ class ViewController: UIViewController {
     
 }
 
+extension ViewController: UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+       return todos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let todo = todos[indexPath.row]
+        
+        cell.textLabel?.text = todo.title
+        
+//        セルに矢印をつける
+        cell.accessoryType = .disclosureIndicator
+        
+        return cell
+    }
+
+//セルがクリックされた時の処理
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        クリックされたTODOをしゅと取得する
+        let todo = todos[indexPath.row]
+        
+        performSegue(withIdentifier: "toNext", sender: todo)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "toNext" {
+//            次の画面のConto次の画面のCotroler次の画面のCotを取得
+            let inputVC = segue.destination as! InputViewController
+            
+//            次の画面に選択されたTodoを設定
+           inputVC.todo = sender as? Todo
+        }
+    }
+}
